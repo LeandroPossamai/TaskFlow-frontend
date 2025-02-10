b77d222bce8280b6d2b99387231382577e205f37
Leandro Possamai <124197652+LeandroPossamai@users.noreply.github.com>
2025-02-10 09:15:43 -0300

    criando a tela de home do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,18 @@ export const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full', // Redireciona para home ao acessar a raiz '/'
+  },
+  {
+    path: '**',
+    redirectTo: 'home', // Redireciona qualquer rota inválida para home
   },
 ];
