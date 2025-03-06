48ce9f3821f109e8f92eb57bde6fece3d3ad72d7
Leandro Possamai <124197652+LeandroPossamai@users.noreply.github.com>
2025-03-06 10:59:03 -0300

    adicionando botao de minimizar dashboard

--- src/app/pages/dashboard-admin/dashboard-admin.component.ts
+++ src/app/pages/dashboard-admin/dashboard-admin.component.ts
@@ -9,6 +9,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard-admin',
@@ -22,6 +24,8 @@ import { MatDividerModule } from '@angular/material/divider';
     MatListModule,
     MatIconModule,
     MatDividerModule,
+    MatTooltipModule,
+    MatButtonModule,
     MatTableModule,
   ],
   templateUrl: './dashboard-admin.component.html',
